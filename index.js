9afcd0eafdb5e9ada06a262c6d56e558678b40aa
LOLSU <reinhardt.alexander@gmail.com>
2020-03-18 18:42:07 +0100

    no github :)

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const fs = require('fs');
 const discord = require('discord.js')
 
 fs.readFile('/home/legendofmiracles/programming/nodejs/webscraper/out.html', 'utf8', function (err, data) {
-  const id = '---';
-  const token = '---'
+  const id = '689718224933486595';
+  const token = 's8KjzvGZFaOVwZL6F1DzJGu8Gxfqg2EZAcd_unAlG7WBZjJ4TPiFYQrCogB0_FczzKmr'
   if (err) throw err;
   var $ = cheerio.load(data);
   var dataFromScrape = $('#myform').find('p').next().first().text().trim();
   console.log(dataFromScrape);
-  var file = fs.readFileSync('/home/user/_tmp/scrape', function (err) {
+  var file = fs.readFileSync('/home/legendofmiracles/_tmp/scrape', function (err) {
     if (err) throw err
   });
   console.log(file.toString().trim());
   if (file != dataFromScrape) {
     console.log("true")
-    new discord.WebhookClient(id, token).send('Neue Hausaufgaben :( Link: ---').catch(console.error);
-    fs.writeFile("/home/user/_tmp/scrape", dataFromScrape, function (err) {
+    new discord.WebhookClient(id, token).send('Neue Hausaufgaben :( Link: http://kkst.s.schule-bw.de/homeoffice/').catch(console.error);
+    fs.writeFile("/home/legendofmiracles/_tmp/scrape", dataFromScrape, function (err) {
       if (err) {
         return console.log(err);
       }
       console.log("The file was saved!");
     });
   } else {
-    fs.writeFile("/home/user/_tmp/scrape", dataFromScrape, function (err) {
+    fs.writeFile("/home/legendofmiracles/_tmp/scrape", dataFromScrape, function (err) {
       if (err) {
         return console.log(err);
       }
--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
-curl -s -d "klassen=08c" --- > out.html
+curl -s -d "klassen=08c" http://kkst.s.schule-bw.de/homeoffice/kontakt.htm > out.html
 node index.js
 wait 20
 killall node
