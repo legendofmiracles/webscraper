714c7f0fa6475e98309f4cd9862392cd7915921c
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-19 09:15:33 +0200

    added eslint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const discord = require('discord.js')
 const os = require('os')
 require('dotenv').config()
+const diff = require('fast-diff')
 fs.readFile(os.homedir()+ '/programming/nodejs/webscraper/out.html', 'utf8', function (err, data) {
   if (err) throw err;
   var $ = cheerio.load(data);
@@ -18,12 +19,16 @@ fs.readFile(os.homedir()+ '/programming/nodejs/webscraper/out.html', 'utf8', fun
   console.log(file.toString().trim());
   if (file != dataFromScrape) {
     console.log("true")
-    fs.writeFile(os.homedir()+ '/scrape/scrape', dataFromScrape, function (err) {
+    fs.writeFileSync(os.homedir()+ '/scrape/scrape', dataFromScrape, function (err) {
       if (err) {
         return console.log(err);
       }
       console.log("The file was saved!");
     });
+
+    var difference = diff(file, dataFromScrape);
+    console.log(difference)
+
     new discord.WebhookClient(id, token).send('Neue Hausaufgaben :( Link: http://kkst.s.schule-bw.de/homeoffice/').catch(console.error);
   } else {
     fs.writeFile(os.homedir() + '/scrape/scrape', dataFromScrape, function (err) {
